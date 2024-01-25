f3c757a85535ff6fb76da2b96bbc7ae9dc5fc9f5
Ildus Sadrtdinov <i.sadrtdinov.1514@mail.ru>
2024-01-25 13:07:44 +0300

    shw 03

---
+++ homeworks-small/shw-03-rnn/model.py
@@ -0,0 +1,70 @@
+import torch
+from typing import Type
+from torch import nn
+from dataset import TextDataset
+
+
+class LanguageModel(nn.Module):
+    def __init__(self, dataset: TextDataset, embed_size: int = 256, hidden_size: int = 256,
+                 rnn_type: Type = nn.RNN, rnn_layers: int = 1):
+        """
+        Model for text generation
+        :param dataset: text data dataset (to extract vocab_size and max_length)
+        :param embed_size: dimensionality of embeddings
+        :param hidden_size: dimensionality of hidden state
+        :param rnn_type: type of RNN layer (nn.RNN or nn.LSTM)
+        :param rnn_layers: number of layers in RNN
+        """
+        super(LanguageModel, self).__init__()
+        self.dataset = dataset  # required for decoding during inference
+        self.vocab_size = dataset.vocab_size
+        self.max_length = dataset.max_length
+
+        """
+        YOUR CODE HERE (⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿
+        Create necessary layers
+        """
+        self.embedding = None
+        self.rnn = None
+        self.linear = None
+
+    def forward(self, indices: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
+        """
+        Compute forward pass through the model and
+        return logits for the next token probabilities
+        :param indices: LongTensor of encoded tokens of size (batch_size, length)
+        :param lengths: LongTensor of lengths of size (batch_size, )
+        :return: FloatTensor of logits of shape (batch_size, length, vocab_size)
+        """
+        # This is a placeholder, you may remove it.
+        logits = torch.randn(
+            indices.shape[0], indices.shape[1], self.vocab_size,
+            device=indices.device
+        )
+        """
+        YOUR CODE HERE (⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿
+        Convert indices to embeddings, pass them through recurrent layers
+        and apply output linear layer to obtain the logits
+        """
+        return logits
+
+    @torch.inference_mode()
+    def inference(self, prefix: str = '', temp: float = 1.) -> str:
+        """
+        Generate new text with an optional prefix
+        :param prefix: prefix to start generation
+        :param temp: sampling temperature
+        :return: generated text
+        """
+        self.eval()
+        # This is a placeholder, you may remove it.
+        generated = prefix + ', а потом купил мужик шляпу, а она ему как раз.'
+        """
+        YOUR CODE HERE (⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿
+        Encode the prefix (do not forget the BOS token!),
+        pass it through the model to accumulate RNN hidden state and
+        generate new tokens sequentially, sampling from categorical distribution,
+        until EOS token or reaching self.max_length.
+        Do not forget to divide predicted logits by temperature before sampling
+        """
+        return generated
---
+++ homeworks-small/shw-03-rnn/train.py
@@ -0,0 +1,132 @@
+import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+from typing import List, Optional, Any
+from torch import nn
+from torch.utils.data import DataLoader
+from IPython.display import clear_output
+from tqdm.notebook import tqdm
+from model import LanguageModel
+
+
+sns.set_style('whitegrid')
+plt.rcParams.update({'font.size': 15})
+
+
+def plot_losses(train_losses: List[float], val_losses: List[float]):
+    """
+    Plot loss and perplexity of train and validation samples
+    :param train_losses: list of train losses at each epoch
+    :param val_losses: list of validation losses at each epoch
+    """
+    clear_output()
+    fig, axs = plt.subplots(1, 2, figsize=(13, 4))
+    axs[0].plot(range(1, len(train_losses) + 1), train_losses, label='train')
+    axs[0].plot(range(1, len(val_losses) + 1), val_losses, label='val')
+    axs[0].set_ylabel('loss')
+
+    """
+    YOUR CODE HERE (⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿
+    Calculate train and validation perplexities given lists of losses
+    """
+    train_perplexities, val_perplexities = [], []
+
+    axs[1].plot(range(1, len(train_perplexities) + 1), train_perplexities, label='train')
+    axs[1].plot(range(1, len(val_perplexities) + 1), val_perplexities, label='val')
+    axs[1].set_ylabel('perplexity')
+
+    for ax in axs:
+        ax.set_xlabel('epoch')
+        ax.legend()
+
+    plt.show()
+
+
+def training_epoch(model: LanguageModel, optimizer: torch.optim.Optimizer, criterion: nn.Module,
+                   loader: DataLoader, tqdm_desc: str):
+    """
+    Process one training epoch
+    :param model: language model to train
+    :param optimizer: optimizer instance
+    :param criterion: loss function class
+    :param loader: training dataloader
+    :param tqdm_desc: progress bar description
+    :return: running train loss
+    """
+    device = next(model.parameters()).device
+    train_loss = 0.0
+
+    model.train()
+    for indices, lengths in tqdm(loader, desc=tqdm_desc):
+        """
+        YOUR CODE HERE (⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿
+        Process one training step: calculate loss,
+        call backward and make one optimizer step.
+        Accumulate sum of losses for different batches in train_loss
+        """
+
+    train_loss /= len(loader.dataset)
+    return train_loss
+
+
+@torch.no_grad()
+def validation_epoch(model: LanguageModel, criterion: nn.Module,
+                     loader: DataLoader, tqdm_desc: str):
+    """
+    Process one validation epoch
+    :param model: language model to validate
+    :param criterion: loss function class
+    :param loader: validation dataloader
+    :param tqdm_desc: progress bar description
+    :return: validation loss
+    """
+    device = next(model.parameters()).device
+    val_loss = 0.0
+
+    model.eval()
+    for indices, lengths in tqdm(loader, desc=tqdm_desc):
+        """
+        YOUR CODE HERE (⊃｡•́‿•̀｡)⊃━✿✿✿✿✿✿
+        Process one validation step: calculate loss.
+        Accumulate sum of losses for different batches in val_loss
+        """
+
+    val_loss /= len(loader.dataset)
+    return val_loss
+
+
+def train(model: LanguageModel, optimizer: torch.optim.Optimizer, scheduler: Optional[Any],
+          train_loader: DataLoader, val_loader: DataLoader, num_epochs: int, num_examples=5):
+    """
+    Train language model for several epochs
+    :param model: language model to train
+    :param optimizer: optimizer instance
+    :param scheduler: optional scheduler
+    :param train_loader: training dataloader
+    :param val_loader: validation dataloader
+    :param num_epochs: number of training epochs
+    :param num_examples: number of generation examples to print after each epoch
+    """
+    train_losses, val_losses = [], []
+    criterion = nn.CrossEntropyLoss(ignore_index=train_loader.dataset.pad_id)
+
+    for epoch in range(1, num_epochs + 1):
+        train_loss = training_epoch(
+            model, optimizer, criterion, train_loader,
+            tqdm_desc=f'Training {epoch}/{num_epochs}'
+        )
+        val_loss = validation_epoch(
+            model, criterion, val_loader,
+            tqdm_desc=f'Validating {epoch}/{num_epochs}'
+        )
+
+        if scheduler is not None:
+            scheduler.step()
+
+        train_losses += [train_loss]
+        val_losses += [val_loss]
+        plot_losses(train_losses, val_losses)
+
+        print('Generation examples:')
+        for _ in range(num_examples):
+            print(model.inference())
